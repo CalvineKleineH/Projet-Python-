8ce8aa9a868ac727b693097fbce48691fa0a027a
CalvineKleineH <calvinekleineabaella@gmail.com>
2023-05-19 02:04:08 +0200

    Utilisation de la fonction sum à la place de l'agorithme

--- calcul.py
+++ calcul.py
@@ -15,11 +15,11 @@ for loop in range(len(moy)):
   list.append((float(incer[i])*float(coeff[i]))/float(moy[i]))
   i+=1
 
-# = sum(list)
+q = sum(list)
 q = 0
 p = 0
-for j in range(len(list)) :
-    q +=float(list[p])
-    p+=1
+#for j in range(len(list)) :
+ #   q +=float(list[p])
+    #p+=1
 num = moy_val * q
 print(f"L'incertitude de {val} est :{num}")
